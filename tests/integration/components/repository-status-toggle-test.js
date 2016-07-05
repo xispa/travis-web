406583e5cae7446667f8f949282c215445afb225
Curtis Ekstrom <code@fauxton.io>
2016-07-05 16:06:16 +0200

    Convert repository-status-toggle test to integration test

---
+++ tests/integration/components/repository-status-toggle-test.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+import { test, moduleForComponent } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('repository-status-toggle', 'RepositoryStatusToggle', {
+  integration: true
+});
+
+test('it renders', function() {
+  let repo = Ember.Object.create({
+    id: 10000,
+    name: "foo-bar",
+    owner_name: "foo",
+    description: "A foo repo",
+    active: true,
+    urlGithub: "https://github.com/foo/foobar",
+    slug: "foo/foo-bar"
+  });
+
+  this.set('repo', repo);
+  this.render(hbs`{{repository-status-toggle hook=repo}}`);
+
+  ok(this.$().find('li.row').hasClass('active'), 'component should have active class');
+  ok(this.$().find('.switch--icon').hasClass('active'), 'switch should have active class');
+  return equal(this.$().find('.profile-repo span').text().trim(), 'A foo repo', 'repo description should be displayed');
+});
